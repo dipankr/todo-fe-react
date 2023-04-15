fdda2a688f0ba620be8a49d15d651388155f8181
dipanker <dipanker_kr@outlook.com>
2023-04-16 00:44:51 +0530

    Updates

---
+++ precache-manifest.7cde4f0d6067f31897f60a5d1e131d95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29de6df17fe082a43721",
+    "url": "/static/css/main.86b4908e.chunk.css"
+  },
+  {
+    "revision": "29de6df17fe082a43721",
+    "url": "/static/js/main.29de6df1.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "d136a1a835855568e8f3",
+    "url": "/static/css/2.bba17908.chunk.css"
+  },
+  {
+    "revision": "d136a1a835855568e8f3",
+    "url": "/static/js/2.d136a1a8.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "31ffc49b95800c17e63b433413379b24",
+    "url": "/index.html"
+  }
+];
